fbc0dd2c69ea729be7df59beef6eebce45def545
Connor <c.meriweather55@gmail.com>
2023-08-30 18:24:27 -0500

    Added case-insenstivity

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -56,12 +56,12 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -75,7 +75,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -87,16 +87,37 @@ public class JobData {
      * Search all columns for the given term
      *
      * @param value The search term to look for
-     * @return      List of all jobs with at least one field containing the value
+     * @return List of all jobs with at least one field containing the value
      */
     public static ArrayList<HashMap<String, String>> findByValue(String value) {
 
-        // load data, if not already loaded
         loadData();
 
-        // TODO - implement this method
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+            // Check if any column's value contains the search term
+            boolean found = false;
+            for (String column : row.keySet()) {
+                String columnValue = row.get(column);
+                if (columnValue.toLowerCase().contains(value.toLowerCase())) {
+                    found = true;
+                    break; // No need to check other columns for this job
+                }
+            }
+
+            if (found) {
+                jobs.add(row);
+            }
+            return jobs;
+        }
         return null;
     }
+
+
+
+
+
 
     /**
      * Read in data from a CSV file and store it in a list
